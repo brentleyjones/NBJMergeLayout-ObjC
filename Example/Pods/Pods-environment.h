fd120af178d075e95b16b99d40e04d6c87b2f7de
Brentley Jones <github@brentleyjones.com>
2014-06-22 17:18:06 -0500

    Convert example to use Cocoapods.
    This way it proves out not only the code, but the fact that the podspec works as well.

---
+++ Example/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// NBJMergeLayout-ObjC
+#define COCOAPODS_POD_AVAILABLE_NBJMergeLayout_ObjC
+#define COCOAPODS_VERSION_MAJOR_NBJMergeLayout_ObjC 1
+#define COCOAPODS_VERSION_MINOR_NBJMergeLayout_ObjC 0
+#define COCOAPODS_VERSION_PATCH_NBJMergeLayout_ObjC 0
+
