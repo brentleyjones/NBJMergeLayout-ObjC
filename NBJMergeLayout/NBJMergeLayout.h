0bf1a0392aa7549bb7d1c7db935f4ebe360832c2
Brentley Jones <Brentley.Jones@target.com>
2014-06-23 09:24:55 -0500

    Add comments

--- NBJMergeLayout/NBJMergeLayout.h
+++ NBJMergeLayout/NBJMergeLayout.h
@@ -20,6 +20,13 @@
 
 #endif
 
+/**
+ *  `NBJMergeLayout` is a view that, when added to a view, adds all of its
+ *  subviews to that view and removes itself. The power of the `NBJMergeLayout`
+ *  view comes when it's used as the root view in a Nib and that Nib is loaded
+ *  as the root view of a custom view or control. In this case it removes an
+ *  unnecessary view from the resulting view hierarchy.
+ */
 @interface NBJMergeLayout : View
 
 @end
